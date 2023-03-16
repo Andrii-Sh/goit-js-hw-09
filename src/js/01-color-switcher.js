aa88c0eeec97dcfae917e869d8ceec4a644fdf32
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-03-16 22:49:10 +0200

    task 01 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+
+stopBtnEl.setAttribute("disabled", "true");
+
+startBtnEl.addEventListener('click', handleStartBtnClick);
+stopBtnEl.addEventListener('click', handleStopBtnClick);
+
+function handleStartBtnClick() {   
+    intervalId = setInterval(() => {
+        changeBgColor();
+    }, 1000);  
+
+    startBtnEl.setAttribute("disabled", "true");
+    stopBtnEl.removeAttribute("disabled");
+}
+
+function handleStopBtnClick() {
+    clearInterval(intervalId);
+    startBtnEl.removeAttribute("disabled");
+    stopBtnEl.setAttribute("disabled", "true");
+}
+
+function changeBgColor() {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
